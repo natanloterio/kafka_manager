6384f7ce919b6abb248b0e4e4c756663a69e2ccb
jeff-beagley <jeff-beagley@mihlfeld.com>
2018-11-13 13:20:13 -0600

    removed unneccessary files

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/server_setup">Server Setup</router-link>
     </div>
     <router-view/>
   </div>
