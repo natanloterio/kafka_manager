a066b9c9782f466ab1d740390da4b30407f190d4
jeff-beagley <jeff-beagley@mihlfeld.com>
2018-11-13 13:29:29 -0600

    Intial bootstrap template setup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-navbar toggleable="md" type="dark" variant="info">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">M&A Kafka Manager</b-navbar-brand>
+
+        <b-collapse is-nav id="nav_collapse">
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-navbar-nav>
+                    <b-nav-item to="/">Home</b-nav-item>
+                    <b-nav-item to="/server_setup">Server Setup</b-nav-item>
+                </b-navbar-nav>
+            </b-navbar-nav>
+
+        </b-collapse>
+    </b-navbar>
+
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
