2088bfff392d6810fc4a9ac074bfeba37e20daf8
jeffbeagley <jeff-beagley@mihlfeld.com>
2018-11-16 15:23:30 -0600

    store now houses separate urls for connector and kafka api urls

--- src/store/index.js
+++ src/store/index.js
@@ -1,52 +1,59 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import * as types from "./mutation-types";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as types from './mutation-types'
 
-Vue.use(Vuex);
-const debug = process.env.NODE_ENV !== "production";
+Vue.use(Vuex)
+const debug = process.env.NODE_ENV !== 'production'
 // mutations
 const mutations = {
-  [types.add_server] (state, { url }) {
-      state.server_url = url.url;
-
-  },
-  [types.remove_server] (state, { url }) {
-      state.server_url = null;
-
-  }
-};
+    [types.add_server] (state, { url, type = 'connect' }) {
+        if(type === 'connect') {
+            state.servers.connectors_api = url
+            return true
+        }
+        state.servers.kafka_api = url
+        return true
+    },
+    [types.remove_server] (state, { type = 'connect' }) {
+        if(type === 'connect') {
+            state.servers.connectors_api = null
+            return true
+        }
+        state.servers.kafka_api = null
+        return true
+    }
+}
 
 // initial state
 const state = {
-  server_url: null,
-  connectors: [],
-  topics: []
+    servers: {
+        'connectors_api': '',
+        'kafka_api': ''
+    },
+    connectors: [],
+    topics: []
 }
 // getters
 const getters = {
-  getServer: state => state.server_url
+    getConnectorServer: state => state.servers.connectors_api,
+    getKafkaServer: state => state.servers.kafka_api
 
-};
+}
 // actions
 const actions = {
-  add_server({ commit }, server_url) {
-    commit(types.add_server, {
-      url: server_url
-    })
-  },
-  remove_server({ commit }, server_url) {
-    commit(types.remove_server, {
-      url: server_url
-    })
-  }
+    add_server ({ commit }, url, type) {
+        commit(types.add_server, url, type)
+    },
+    remove_server ({ commit }, type) {
+        commit(types.remove_server, type)
+    }
 }
-
 
 // one store for entire application
 export default new Vuex.Store({
-  state,
-  strict: debug,
-  getters,
-  actions,
-  mutations
-});
+    state,
+    strict: debug,
+    getters,
+    actions,
+    mutations
+})
--- src/views/server_setup.vue
+++ src/views/server_setup.vue
@@ -3,15 +3,24 @@
     <b-container>
         <h1 class="mt-5">Server Setup</h1>
         <p class="lead">Adding your server url here will cause it to persist across this app for this session only</p>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="connect_url" 
-                    label="Kafka Connect URL:"
+    <b-form @submit="setServers" @reset="onReset" v-if="show">
+      <b-form-group id="connect_url"
+                    label="Connect URL:"
                     label-for="exampleInput1"
-                    description="This is the URL that points to a Kafka Connect Instance.. by default it will be http://hostname:8083 tip - do not use a trailing slash">
+                    description="This is the URL that points to a Kafka Connect Instance.. by default it will be http://hostname:8083">
         <b-form-input id="exampleInput1"
                       type="text"
-                      v-model="form.url"
-                      required
+                      v-model="form.connect_url"
+                      placeholder="Enter url">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="connect_url"
+                    label="Kafka REST URL:"
+                    label-for="exampleInput1"
+                    description="This is the URL that points to a Kafka Connect Instance.. by default it will be http://hostname:8082">
+        <b-form-input id="exampleInput1"
+                      type="text"
+                      v-model="form.kafka_url"
                       placeholder="Enter url">
         </b-form-input>
       </b-form-group>
@@ -27,36 +36,48 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
 export default {
-  data () {
-    return {
-      form: {
-        url: ''
-      },
-      show: true
-    }
-  },
-  methods: {
-    ...mapActions(['add_server']),
-    onSubmit (evt) {
-        evt.preventDefault();
-
-        this.$store.dispatch('add_server', {
-            url: this.form.url
-        
-        });
-
+    data () {
+        return {
+            form: {
+                url: ''
+            },
+            show: true
+        }
     },
-    onReset (evt) {
-      evt.preventDefault();
-      /* Reset our form values */
-      this.form.url = '';
+    methods: {
+        ...mapActions(['add_server']),
+        setServers (evt) {
+            evt.preventDefault()
 
-    this.$store.dispatch('remove_server');
+            if(this.form.connect_url !== '') {
+                this.$store.dispatch('add_server', {
+                    url: this.form.connect_url,
+                    type: 'connect'
 
-      /* Trick to reset/clear native browser form validation state */
-      this.show = false;
-      this.$nextTick(() => { this.show = true });
+                })
+            }
+
+            if(this.form.kafka_url !== '') {
+                this.$store.dispatch('add_server', {
+                    url: this.form.kafka_url,
+                    type: 'kafka'
+
+                })
+            }
+        },
+        onReset (evt) {
+            evt.preventDefault()
+            /* Reset our form values */
+            this.form.kafka_url = ''
+            this.form.connect_url = ''
+
+            this.$store.dispatch('remove_server', { type: 'connect' })
+            this.$store.dispatch('remove_server', { type: 'kafka' })
+
+            /* Trick to reset/clear native browser form validation state */
+            this.show = false
+            this.$nextTick(() => { this.show = true })
+        }
     }
-  }
 }
 </script>
