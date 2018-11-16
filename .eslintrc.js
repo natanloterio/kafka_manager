cd3d917e7f08a7eb830607832a4babc7c18f666b
jeffbeagley <jeff-beagley@mihlfeld.com>
2018-11-16 15:23:02 -0600

    eslint config option changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "indent": ["error", 4, { "MemberExpression": 1 }],
+    "keyword-spacing": ["error", { "overrides": {
+        "if": { "after": false },
+        "for": { "after": false },
+        "while": { "after": false }
+    } }]
   },
   parserOptions: {
     parser: 'babel-eslint'
