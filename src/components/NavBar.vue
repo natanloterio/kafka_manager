b5ec1db88fb9b04ff37ff84329b6544f77ef3ad4
jeffbeagley <jeff-beagley@mihlfeld.com>
2018-11-16 15:23:11 -0600

    got rid of server status in navbar for now

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,12 +4,11 @@
         <b-navbar-brand to="/">M&A Kafka Manager</b-navbar-brand>
 
         <b-collapse is-nav id="nav_collapse">
-            <!-- Right aligned nav items -->
-            Server: {{current_server}}
             <b-navbar-nav class="ml-auto">
                 <b-navbar-nav>
                     <b-nav-item to="/">Home</b-nav-item>
                     <b-nav-item to="/server_setup">Server Setup</b-nav-item>
+                    <b-nav-item to="/topics">Topics</b-nav-item>
                     <b-nav-item to="/connectors">Connectors</b-nav-item>
                 </b-navbar-nav>
             </b-navbar-nav>
@@ -21,22 +20,12 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-import check_server from '../modules/check_server_connection'
 
 export default {
     name: 'NavBar',
     computed: {
-        current_server(){
-            return this.$store.getters.getServer;
-        },
-        connection_status_icon(){
-            if(this.current_server === null) {
-                return;
-
-            }
-
-            
+        current_server () {
+            return this.$store.getters.getConnectorServer
         }
     }
 }
