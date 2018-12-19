1a85928666f4a8403953c5e1ccff89a89839493e
jeffbeagley <jeffbeagley@gmail.com>
2018-12-19 17:26:02 -0600

    turned off camelcase alert

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'camelcase': 'off',
     "indent": ["error", 4, { "MemberExpression": 1 }],
     "keyword-spacing": ["error", { "overrides": {
         "if": { "after": false },
