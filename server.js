2519b4724b13316105333f36f26e1b05f99c0b74
jeffbeagley <jeffbeagley@gmail.com>
2018-12-27 17:59:50 -0600

    setup server.js to act as an API and serve static files in one

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.use(express.static('dist'))
+const dist = path.join(__dirname, 'dist')
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(dist, 'index.html'))
+})
+
+app.get('/status', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
+app.use('/', express.static(dist))
 
 const port = process.env.PORT || 8080
 
