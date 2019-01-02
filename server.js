b865d767d69d58b0c565ca68dbc5224f9eedc833
jeffbeagley <jeffbeagley@gmail.com>
2019-01-02 11:21:51 -0600

    added npm script to run local api when developing

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const kafka = require('kafka-node')
+const bodyParser = require('body-parser')
 const dist = path.join(__dirname, 'dist')
+const kafkaHost = process.env.KAFKA_SERVERS || '10.192.172.105:32400'
+const cors = require('cors')
+
+const client = new kafka.KafkaClient({ kafkaHost: kafkaHost })
+
+client.on('error', function (err) {
+    console.log(err)
+})
+
+client.once('connect', function () {
+    console.log('connected')
+    client.loadMetadataForTopics([], function (error, results) {
+        if(error) {
+            return console.error(error)
+        }
+    })
+})
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(dist, 'index.html'))
@@ -12,9 +34,42 @@ app.get('/status', function (req, res) {
     res.send('GET request to the homepage')
 })
 
+app.post('/topic', function (req, res) {
+    let e = []
+
+    if(typeof req.body === 'undefined') { req.body = {} }
+    if(typeof req.body.topic === 'undefined') { e.push(`parameter [topic] required`) }
+    if(typeof req.body.partitions === 'undefined') { e.push(`parameter [partitions] required`) }
+    if(typeof req.body.replication === 'undefined') { e.push(`parameter [replication] required`) }
+
+    if(e.length > 0) {
+        return res.send(e.join('<br>'))
+    }
+
+    let topic = req.body.topic
+    let partitions = req.body.partitions
+    let replicationFactor = req.body.replication
+
+    var topics = [{
+        topic: topic,
+        partitions: partitions,
+        replicationFactor: replicationFactor
+    }]
+
+    const admin = new kafka.Admin(client)
+
+    admin.createTopics(topics, (err, response) => {
+        if(err) {
+            return res.send(err)
+        }
+
+        return res.send(response)
+    })
+})
+
 app.use('/', express.static(dist))
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 
 app.listen(port, function () {
     console.log('Kafka Manager started and is accessible via http://localhost:%s', port)
--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,13 @@ Vue.use(Vuex)
 let connectServer = 'http://connect.kafka.10.192.172.103.xip.io'
 let ksqlServer = 'http://ksql-server.kafka.10.192.172.103.xip.io/'
 let restServer = 'http://localhost:8084'
+let apiServer = 'http://localhost:8081'
 
 if(process.env.NODE_ENV === 'production') {
     connectServer = process.env.VUE_APP_CONNECT_SERVER
     restServer = process.env.VUE_APP_REST_SERVER
     ksqlServer = process.env.VUE_APP_KSQL_SERVER
+    apiServer = 'http://localhost'
 }
 
 // fix urls with trailing slashes
@@ -30,14 +32,16 @@ const state = {
     servers: {
         'connectors_api': '',
         'kafka_api': '',
-        'ksql_api': ''
+        'ksql_api': '',
+        'api': ''
     }
 }
 // getters
 const getters = {
     getConnectorServer: state => state.servers.connectors_api,
     getKafkaServer: state => state.servers.kafka_api,
-    getKsqlServer: state => state.servers.ksql_api
+    getKsqlServer: state => state.servers.ksql_api,
+    getApiServer: state => state.servers.api
 
 }
 // actions
@@ -49,6 +53,7 @@ const actions = {
 state.servers.connectors_api = connectServer
 state.servers.kafka_api = restServer
 state.servers.ksql_api = ksqlServer
+state.servers.api = apiServer
 
 // one store for entire application
 export default new Vuex.Store({
